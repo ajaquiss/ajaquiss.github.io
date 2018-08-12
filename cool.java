f0c94f1372ff3815bf98e42b3e8d50f9af958aa6
Thomas Kearney <tom.f.kearney@gmail.com>
2018-08-12 02:35:18 -0400

    why was this here originally?

--- cool.java
+++
@@ -1,5 +0,0 @@
-
-public interface cool{
-  void isItCool();
-  void isItSafe();
-}
--- twoDee.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class twoDee{
-  public static void main(String[] args){
-    // Scanner in = new Scanner(System.in);
-    int[][] Ar= new int[2][2];
-    Ar[0][0]=0;
-    System.out.println(""+Ar[0][0]);
-  }
-}
